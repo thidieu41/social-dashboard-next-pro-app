1144995e36944c61c7ac182c38ca85fc327c4aa8
theresa <theresa@iptp.vn>
2025-10-31 13:25:37 +0700

    chore: add all files media

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // dùng class dark, không phải media
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
