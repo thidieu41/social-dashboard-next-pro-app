7b8208bb7513f5387e2b7013f0f62974252c68c2
theresa <theresa@iptp.vn>
2025-10-31 13:58:12 +0700

    add bg login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,8 +2,8 @@ import FormLogin from "./login-form";
 
 const Login = () => {
   return (
-    <div className=" w-screen h-screen bg-[url(/auth/bg.avif)] bg-cover bg-center bg-no-repeat relative">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[500px] bg-white/90 p-6 rounded-2xl shadow-xl bg-opacity-50">
+    <div className=" w-screen h-screen bg-[url(/auth/bg-auth.jpg)] bg-cover bg-center bg-no-repeat relative">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[500px] bg-white/95 p-6 rounded-2xl shadow-xl bg-opacity-50">
         <div className="flex flex-col gap-2 mb-2">
           <h1 className=" text-2xl font-bold">Welcome back!</h1>
           <p className="text-sm/7 text-gray-700 ">
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("nextToken")?.value;
+  // const token = request.cookies.get("nextToken")?.value;
+  const token = false;
   const pathname = request.nextUrl.pathname;
 
-  // Nếu chưa có token => redirect đến /login
-  if (!token && !pathname.startsWith("/login")) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (token && pathname === "/login") {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  console.log(pathname, token);
+  if (token) {
+    if (!pathname.startsWith("/login") || !pathname) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  } else {
+    if (pathname === "/login") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
   }
 
   // Nếu có token => cho vào app
